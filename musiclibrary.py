bc7b3fd2d44e45578fa040ed0231f7ce9553629e
Kellax-ux <makkartanishq41@gmail.com>
2025-02-16 21:15:08 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "navaan":"https://youtu.be/pLdP6kUy4IQ",
+    "lalkara": "https://youtu.be/-mgGnx1p3b8",
+    "karan": "https://youtu.be/XTp5jaRU3Ws",
+    "dhani": "https://youtu.be/J5aSzRDjIqQ"
+}
